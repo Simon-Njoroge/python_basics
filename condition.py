db70c995c02dcef26919a153d354093a3f395bdf
simon njoroge <mukirisimon22@gmail.com>
2024-10-28 16:55:08 +0300

    initial

--- condition.py
+++ condition.py
@@ -1,15 +1,23 @@
 maths = 50
 english = 100
-from fuction import sum
-if maths > english:
-    print("maths is greater")
-elif maths<english:
-    print("math is  less")
+kiswahili =80
+bio=90
+chem=89
+comp=10
+hist=90
+agri=120
+av=(maths+english+kiswahili+bio+chem+comp+hist+agri)/8
+print(av)
+
+if av<=100 and av>=80:
+   print("A")
+elif av<=79 and av>=60:
+   print("B")
+elif av<=59 and av>=50:
+   print("C")
+elif av<=49 and av>=40:
+   print("D")
+elif av<=39 and  av>=0:
+   print("E")
 else:
-    print("maths is equal")
-y= int(input("Enter a number: "))
-if y>0:
-    print(f"{y} is postive")
-else:
-    print|(f"{y} is negative")
-sum(2,2)
+   print("invalid")
