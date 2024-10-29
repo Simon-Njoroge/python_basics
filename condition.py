5d926bbe2bc7fff014f533fd0596aeea28a38954
simon njoroge <mukirisimon22@gmail.com>
2024-10-29 19:42:23 +0300

    initial

--- condition.py
+++ condition.py
@@ -1,23 +1,27 @@
-maths = 50
-english = 100
-kiswahili =80
-bio=90
-chem=89
-comp=10
-hist=90
-agri=120
-av=(maths+english+kiswahili+bio+chem+comp+hist+agri)/8
-print(av)
+# maths = 50
+# english = 100
+# kiswahili =80
+# bio=90
+# chem=89
+# comp=10
+# hist=90
+# agri=120
+# av=(maths+english+kiswahili+bio+chem+comp+hist+agri)/8
+# print(av)
+#
+# if av<=100 and av>=80:
+#    print("A")
+# elif av<=79 and av>=60:
+#    print("B")
+# elif av<=59 and av>=50:
+#    print("C")
+# elif av<=49 and av>=40:
+#    print("D")
+# elif av<=39 and  av>=0:
+#    print("E")
+# else:
+#    print
+students = ["jane","ann","joy","caleb","joy"]
 
-if av<=100 and av>=80:
-   print("A")
-elif av<=79 and av>=60:
-   print("B")
-elif av<=59 and av>=50:
-   print("C")
-elif av<=49 and av>=40:
-   print("D")
-elif av<=39 and  av>=0:
-   print("E")
-else:
-   print("invalid")
+last=len(students)-1
+print(students[last])
