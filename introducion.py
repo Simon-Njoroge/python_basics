f103e0285bba658ba7055cfeaedf62fd624e1128
simon njoroge <mukirisimon22@gmail.com>
2024-10-30 11:16:21 +0300

    initial

---
+++ introducion.py
@@ -0,0 +1,21 @@
+age = int(input("age: "))
+print(type(age))
+
+name="jennifer"
+print(name[1:-1])
+
+first="jane"
+second="wangari"
+
+print(f"my name is {first} {second}")
+
+print(first.upper())
+print(first.lower())
+print(first.find('a'))
+print(first.replace("jane","joy"))
+print("jane" in first)
+print(age / 3)
+print(age // 3)
+print(age % 3)
+print(age**2)
+print((age)+(2+2))
